1203684c3dce642cd0e8b0d9702925ae2fd58c91
NZTK <tkm.2438nishizawa@gmail.com>
2019-05-09 16:15:11 +0900

    [Start]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,21 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blogs = Blog.new
   end
 
   def edit
+  end
+
+  def create
+  	@blogs = blog.new(blog_params)
+  	blog.save
+  	redirect_to blog_path
+  end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :category,:body)
+  	
   end
 end
